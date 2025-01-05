f6167efad23b62d79636e8cad2007893a88edecd
Ayush1519 <singhayush6872@gmail.com>
2025-01-05 15:17:52 +0000

    using function to count vowels

--- Functions/Basic_questions.c
+++ Functions/Basic_questions.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 void slice(char str[], int n, int m);
+int countVowel(char str[]);
 int main() {
     char str[] = "HappyJourney";
     slice(str,2,8);
+    printf("Vowel is: %d",countVowel(str));
 }
 void slice(char str[], int n, int m) {
     char newstr[100];
@@ -13,3 +15,14 @@ void slice(char str[], int n, int m) {
     newstr[j]='\0';
     puts(newstr);
 }
+
+int countVowel(char str[]) {
+    int count = 0;
+
+    for(int i=0;str[i] != '\0';i++){
+        if(str[i] == 'a'||str[i] == 'e'||str[i] == 'i'||str[i] == 'o'||str[i] == 'u'){
+            count++;
+        }
+    }
+    return count;
+}    
