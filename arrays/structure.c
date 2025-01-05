ad27fb33896a5614d5a7e01dfa7e10f4f3534c11
Ayush1519 <singhayush6872@gmail.com>
2025-01-05 08:20:23 +0000

    using strlen(To count number of characters excludeing \0)

--- arrays/structure.c
+++ arrays/structure.c
@@ -1,13 +1,34 @@
 #include<stdio.h>
 #include<string.h>
 int strcmp();
+int countLength(char arr[]);
+int countLength(char arr[]) {
+    int count = 0;
+    for(int i=0; arr[i] != '\0'; i++) {
+        count++;
+    }
+    return count-1;
+}
+
 int main() {
     char firststr[] = "Apple";
     char secstr[] = "Banana";
     printf("%d\n", strcmp(firststr,secstr));
     printf("%d\n",strcmp(secstr,firststr));
+
     char oldstr[] = "oldstr";
     char newstr[] = "newstr";
     strcpy(newstr,oldstr);
     puts(newstr);
+
+    char first1str[100] = "Good";
+    char secondstr[] = "Morning";
+    strcat(first1str,secondstr);
+    puts(first1str);
+    
+    char name[] = "Ayush";
+    int length = strlen(name);
+    printf("Length is: %d",length);
+    return 0;
 }
+
