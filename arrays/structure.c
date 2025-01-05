0474942709a536fbccd3f439d0b3c03082526cf9
Ayush1519 <singhayush6872@gmail.com>
2025-01-05 08:03:25 +0000

    using strcpy(copies values of old string to new)

--- arrays/structure.c
+++ arrays/structure.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+#include<string.h>
 int strcmp();
 int main() {
     char firststr[] = "Apple";
     char secstr[] = "Banana";
     printf("%d\n", strcmp(firststr,secstr));
     printf("%d\n",strcmp(secstr,firststr));
+    char oldstr[] = "oldstr";
+    char newstr[] = "newstr";
+    strcpy(newstr,oldstr);
+    puts(newstr);
 }
