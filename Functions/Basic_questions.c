4ef6ab013d43283cd68d2902a125fd92abd713d1
Ayush1519 <singhayush6872@gmail.com>
2025-01-05 15:26:00 +0000

    String input from the user

--- Functions/Basic_questions.c
+++ Functions/Basic_questions.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
-void slice(char str[], int n, int m);
+void slice(char str1[], int n, int m);
 int countVowel(char str[]);
 int main() {
+    char str[100];
+    char ch;
+    int i=0;
+    while(ch != '\n'){
+        scanf("%c", &ch);
+        str[i] = ch;
+        i++;
+    }
+    str[i] = '\0';
+    puts(str);
     char str[] = "HappyJourney";
     slice(str,2,8);
     printf("Vowel is: %d",countVowel(str));
