77eaced63e5e604bca7f9c3f5c69c74148a12c0f
Ayush1519 <singhayush6872@gmail.com>
2025-01-13 03:43:09 +0000

    using for loop to print the numbers in reverse

--- Functions/Ayushworkspace.c
+++ Functions/Ayushworkspace.c
@@ -29,6 +29,10 @@ age>=18?printf("Is an adult"):printf("Not an adult");
 for(int i=1;i<=10;i++){
     printf("%d\n",i);
 }
+
+for(int i=10;i>=1;i--){
+    printf("%d\n",i);
+}
         return 0;
         
     }
