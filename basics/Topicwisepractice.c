d65def6b203499cc4ce5eda79fb6ce69906d9bf6
Ayush1519 <singhayush6872@gmail.com>
2025-01-18 07:44:25 +0000

    printing fibonacci series using arrays

--- basics/Topicwisepractice.c
+++ basics/Topicwisepractice.c
@@ -89,6 +89,35 @@ void reverse(int *arr, int n) {
     arr[n-i-1]=firstval;
     }
 }
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    
+    // Prompt user for the number of terms
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    
+    // Declare an array to store Fibonacci series
+    int fib[n];
+    
+    // Initialize the first two terms
+    fib[0] = 0;
+    fib[1] = 1;
+    
+    // Generate the Fibonacci series
+    for (i = 2; i < n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    
+    // Print the Fibonacci series
+    printf("Fibonacci Series: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", fib[i]);
+    }
+    
+    return 0;
+}
 
 
 void function()
