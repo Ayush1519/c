d05b583c0e592a257004b86e0b1d039dd77073ad
Ayush1519 <singhayush6872@gmail.com>
2025-01-23 09:07:16 +0000

    using functions to print table

--- Functions/Ayushworkspace.c
+++ Functions/Ayushworkspace.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 void printString(char arr[]);
+void printTable(int n);
 int main() {
        int a=5;
        int b=6;
@@ -183,5 +184,17 @@ void printString(char arr[]){
     // Close the file after reading
     fclose(file);
 
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printTable(n);
+
     return 0;
+}
+
+void printTable(int n){
+       for(int i=1; i<=10; i++){
+              printf("%d\n", n*i);
+       }
 }
