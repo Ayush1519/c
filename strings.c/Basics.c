cf3c93f00194c5d083cacdb0b1dbc25a3fdf72d1
Ayush1519 <singhayush6872@gmail.com>
2025-01-28 08:05:44 +0000

    code practice

--- strings.c/Basics.c
+++ strings.c/Basics.c
@@ -1,42 +1,74 @@
 #include <stdio.h>
 
 int main() {
-    // Define variables
+    // // Define variables
+    // char name[50];
+    // int age;
+    // float height;
+
+    // // Open file for writing
+    // FILE *file = fopen("person.txt", "w");
+    // if (file == NULL) {
+    //     printf("Error opening file for writing.\n");
+    //     return 1;
+    // }
+
+    // // Assign values to variables
+    // snprintf(name, sizeof(name), "Ayush singh");
+    // age = 19;
+    // height = 5.9;
+
+    // // Write data to file
+    // fprintf(file, "Name: %s\nAge: %d\nHeight: %.1f\n", name, age, height);
+    // fclose(file);
+
+    // // Open file for reading
+    // file = fopen("person.txt", "r");
+    // if (file == NULL) {
+    //     printf("Error opening file for reading.\n");
+    //     return 1;
+    // }
+
+    // // Read data from file
+    // fscanf(file, "Name: %s\nAge: %d\nHeight: %f\n", name, &age, &height);
+    // fclose(file);
+
+    // // Print the read data
+    // printf("Read from file:\n");
+    // printf("Name: %s\n", name);
+    // printf("Age: %d\n", age);
+    // printf("Height: %.1f\n", height);
+    
     char name[50];
     int age;
     float height;
 
-    // Open file for writing
-    FILE *file = fopen("person.txt", "w");
-    if (file == NULL) {
+    FILE *file= fopen("person.text", "w");
+    if(file==NULL){
         printf("Error opening file for writing.\n");
         return 1;
     }
 
-    // Assign values to variables
-    snprintf(name, sizeof(name), "Ayush singh");
+    snprintf(name ,sizeof(name), "Ayush singh");
     age = 19;
     height = 5.9;
 
-    // Write data to file
-    fprintf(file, "Name: %s\nAge: %d\nHeight: %.1f\n", name, age, height);
+    fprintf(file,"Name: %s\nAge: %d\nHeight: %f\n", name, age, height);
     fclose(file);
 
-    // Open file for reading
-    file = fopen("person.txt", "r");
-    if (file == NULL) {
-        printf("Error opening file for reading.\n");
+
+    file= fopen("person.text", "r");
+    if(file == NULL){
+        printf("Error in opening file for reading.");
         return 1;
     }
 
-    // Read data from file
-    fscanf(file, "Name: %s\nAge: %d\nHeight: %f\n", name, &age, &height);
+    fscanf(file," Name: %s\nAge: %d\nHeight: %f\n", name, &age, &height);
     fclose(file);
 
-    // Print the read data
     printf("Read from file:\n");
-    printf("Name: %s\n", name);
-    printf("Age: %d\n", age);
+    printf("Name: %s\n",name);
+    printf("Age: %c\n", age);
     printf("Height: %.1f\n", height);
 
     return 0;
