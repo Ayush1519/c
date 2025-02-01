2ad8a59a56b7f9856e20c742a7ab4b1226475f3b
Ayush1519 <singhayush6872@gmail.com>
2025-02-01 12:18:37 +0000

    using incerment and decrement operator in floating type value for address

--- Functions/Ayushworkspace.c
+++ Functions/Ayushworkspace.c
@@ -32,6 +32,13 @@ int main() {
        printf("p++ = %u\n", p);
        p--;
        printf("p-- = %u\n", p);
+       float b = 22.22;
+       float *q = &b;
+       printf("q = %u\n", q);  
+       q++;
+       printf("q++ = %u\n", q); 
+       q--;
+       printf("q-- = %u\n", q);
        int substraction=a-b;
        int multiplication=a*b;
        int division=a/b;
