2c2c9a0cc09fba9eb54fd3c830db52a00f1fdcde
Ayush1519 <singhayush6872@gmail.com>
2025-03-08 02:59:28 +0000

    using for loop to print patterns in reverse

--- basics/loops.c
+++ basics/loops.c
@@ -10,6 +10,13 @@ return n * factorial(n-1);//returns factorial
 }
 
 int main(){
+    int n=4;
+    for(int i=n; i>=1; i--){
+        for(int j=0; j<i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
     for(int i=1;i<6;i++){//loop for printing opposite triangle
         for(int j=1;j<i;j++){
             printf("%s","*");
