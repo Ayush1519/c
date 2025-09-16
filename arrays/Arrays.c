78f0b51720986e2e0f73669e77c78c03662ee017
Ayush1519 <singhayush6872@gmail.com>
2025-09-16 15:01:15 +0530

    Update Arrays.c

--- arrays/Arrays.c
+++ arrays/Arrays.c
@@ -22,5 +22,54 @@ int main() {
     printf("Maximum element = %d\n", max);
     printf("Minimum element = %d\n", min);
 
+    
+    //Addition and multiplication of 3*3 matrix
+    int matrix1[3][3], matrix2[3][3], sum[3][3], multiplication[3][3];
+    int i, j, k;
+
+    printf("Enter elements of first 3x3 matrix:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("\nEnter elements of second 3x3 matrix:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            multiplication[i][j] = 0;
+            for(k = 0; k < 3; k++) {
+                multiplication[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    printf("\nSum of the two matrices:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nMultiplication of the two matrices:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d\t", multiplication[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
